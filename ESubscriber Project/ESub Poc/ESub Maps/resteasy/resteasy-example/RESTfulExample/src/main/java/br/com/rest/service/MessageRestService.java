c0d93bc1d5f8459065e95f56b459363132802040
Guilherme <tr_gbueno@gbueno-Latitude-E4300.(none)>
2012-07-20 11:18:56 -0300

    synch

--- ESubscriber Project/ESub Poc/ESub Maps/resteasy/resteasy-example/RESTfulExample/src/main/java/br/com/rest/service/MessageRestService.java
+++ ESubscriber Project/ESub Poc/ESub Maps/resteasy/resteasy-example/RESTfulExample/src/main/java/br/com/rest/service/MessageRestService.java
@@ -1,9 +1,13 @@
 package br.com.rest.service;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+
+import org.apache.commons.lang.StringUtils;
 import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
@@ -13,32 +17,47 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.rest.service.model.DistanceMatrixResponse;
  
+/**
+ * Chamada de exemplo: http://localhost:8080/rest/message/rua Roberto Simonsen, campinas, São Paulo/para/Francisco Glicério, Campinas - São Paulo
+ * @author tr_gbueno
+ *
+ */
 @Path("/message")
 public class MessageRestService {
 	static {
-		// precisa ser chamo uma única vez para registrar providers RESTEasy, scanear classes, etc
-		 RegisterBuiltin.register(ResteasyProviderFactory.getInstance()); // precisa ser chamado uma única vez para
+		 RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
 	}
+	
+	private static final String DEFAULT_MODE="driving";
+	private static final String DEFAULT_LANGUAGE="pt-BR";
+	private static final String DEFAULT_SENSOR="false";
  
 	@GET
 	@Path("/{origem}/para/{destino}")
-	/*
-	 * http://localhost:8080/rest/message/Guilherme/para/bueno
-	*/
 	public Response printMessage(@PathParam("origem") String origem, @PathParam("destino") String destino) {
 		String result = "Restful example : " + origem + " - "+ destino;
+		DistanceMatrixResponse resultado = getDistancia(origem, destino);
+		return Response.status(200).entity(resultado).build();
+	}
+	
+	private DistanceMatrixResponse getDistancia(List<String> origens, List<String> destinos){
+		String origem = StringUtils.join(origens.iterator(), "|");
+		String destino = StringUtils.join(destinos.iterator(), "|");
+		DistanceMatrixResponse resultado = getDistancia(origem, destino);
+		return resultado;
+	}
+	
+	private DistanceMatrixResponse getDistancia(String origem, String destino){
 		DistanceMatrixResponse resultado = new DistanceMatrixResponse();
 		try {
-			ClientRequest request = new ClientRequest("http://maps.googleapis.com/maps/api/distancematrix/xml?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Vancouver+BC&mode=bicycling&language=pt-BR&sensor=false");
+			ClientRequest request = new ClientRequest("http://maps.googleapis.com/maps/api/distancematrix/xml?origins="+origem+"&destinations="+destino+"&mode="+DEFAULT_MODE+"&language="+DEFAULT_LANGUAGE+"&sensor="+DEFAULT_SENSOR);
 			System.out.println(request.get().getEntity(String.class));
 			ClientResponse<DistanceMatrixResponse> response = request.get(DistanceMatrixResponse.class);
 			System.out.println(resultado = response.getEntity());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		return Response.status(200).entity(result +" Resultado: "+resultado.getDestination_address()).build();
+		return resultado;
 	}
  
 }
