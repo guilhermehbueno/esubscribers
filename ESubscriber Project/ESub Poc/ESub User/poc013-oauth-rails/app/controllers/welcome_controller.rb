95d8370d5def181762803197d97074622ceaa832
Guilherme <tr_gbueno@gbueno-Latitude-E4300.(none)>
2012-08-16 11:27:17 -0300

    Adicionando Koala

--- ESubscriber Project/ESub Poc/ESub User/poc013-oauth-rails/app/controllers/welcome_controller.rb
+++ ESubscriber Project/ESub Poc/ESub User/poc013-oauth-rails/app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
 	def index
+		@user = current_user
+		if @user
+			@graph = Koala::Facebook::API.new(@user.oauth_token)
+			@friends = @graph.get_connections("me", "friends")
+		end
 	end
 end
