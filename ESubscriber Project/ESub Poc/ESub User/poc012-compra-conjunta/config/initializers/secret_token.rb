f68861625b02812b722290cd3a25ed4e085fdd71
Guilherme <guilherme@guilherme-HP-Pavilion-dv4-Notebook-PC.(none)>
2012-08-19 07:32:34 -0300

    Adicionando app com mongodb e omniauth para o facebook

---
+++ ESubscriber Project/ESub Poc/ESub User/poc012-compra-conjunta/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poc012CompraConjunta::Application.config.secret_token = '0624d15b5643e10a89994741e2cc61157aee1ea568b20b411167cf03944a8e2774eef0ce8c3958047c14685998d6033efac7329bfbb49c38cb4ba5d7969ddcaa'
